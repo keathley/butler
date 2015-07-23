344a3c2a5b27c1e37f9d68c40e0163a3b87ff279
Caleb Albritton <ace@fastmail.fm>
2015-07-22 20:43:59 -0400

    added http library and sample calls in the butler.ex

--- lib/butler.ex
+++ lib/butler.ex
@@ -1,2 +1,12 @@
 defmodule Butler do
+
+  client_id = "xoxb-8072126112-SO33tzVKXOjuNnd4BLsb6d1r"
+  team = "chadev"
+  state = "crunk"
+  url = "htpps://slack.com/oauth/authorize?client_id"<>client_id<>"&team="<>team<>"&state="<>state
+  IO.puts url
+  HTTPoison.start
+  response = HTTPoison.get! "http://ip.jsontest.com/"
+  
+  #ip_pack = Poison.Parser.parse!()
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule Butler.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [
+      :logger,
+      :httpoison
+    ]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +30,10 @@ defmodule Butler.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:poison, "~> 1.4.0"},
+      {:socket, "~> 0.2.8"},
+      {:httpoison, "~> 0.7"}
+    ]
   end
 end
