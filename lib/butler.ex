9384be354b8832b0807813dbd2f9db9037a6faa0
Chris Keathley <c@keathley.io>
2015-07-22 22:06:44 -0400

    The best laid plans of mice and men.

--- lib/butler.ex
+++ lib/butler.ex
@@ -1,23 +1,33 @@
 defmodule Butler do
-  HTTPoison.start
 
-  client_id = System.get_env "BUTLER_KEY"
-  # team = "chadev"
-  # state = "crunk"
-  start_url = "https://slack.com/api/rtm.start?token="<>client_id
+  def start_link do
+    HTTPoison.start
 
-  case HTTPoison.get(start_url) do
-    {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-      Butler.get_url body
-      # IO.puts body
-    {:error, %HTTPoison.Error{reason: reason}} ->
-      IO.inspect reason
+    client_id = System.get_env "BUTLER_KEY"
+    start_url = "https://slack.com/api/rtm.start?token="<>client_id
+
+    case HTTPoison.get(start_url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+        |> get_url
+        # |> connect_to_socket
+        # |> read_from_socket
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
   end
 
-  #ip_pack = Poison.Parser.parse!()
-
-  def get_url body do
+  defp get_url body do
     %{"url" => url } = Poison.Parser.parse! body
     IO.puts url
+    url
+  end
+
+  defp connect_to_socket url do
+    socket = Socket.Web.connect! url
+  end
+
+  defp read_from_socket socket do
+    IO.puts(Socket.Web.recv!(socket))
   end
 end
