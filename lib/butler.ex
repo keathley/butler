5d4171a9b4c25fa791f25ca5946cf0047853bef8
Caleb Albritton <ace@fastmail.fm>
2015-07-30 22:45:55 -0400

    now takes the token as a --token= argument and compiles down to an executable

--- lib/butler.ex
+++ lib/butler.ex
@@ -1,25 +1,37 @@
 defmodule Butler do
 
-  def start_link do
+  def main(args) do
     HTTPoison.start
 
-    client_id = System.get_env "BUTLER_KEY"
-    start_url = "https://slack.com/api/rtm.start?token="<>client_id
+    options = args |> parse_args
+    token = options[:token]
+    IO.puts "Connection to ChaDev Slack with Token: #{token}"
+    start_url = "https://slack.com/api/rtm.start?token=#{token}"
 
     case HTTPoison.get(start_url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        body
-        |> get_url
-        # |> connect_to_socket
-        # |> read_from_socket
+        body |> get_url |> connect_to_socket |> read_from_socket
+
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.inspect reason
     end
   end
 
+  def parse_args([]) do
+    IO.puts "Required a Slack Bot Token"
+  end
+
+  def parse_args(args) do
+    { options, _, _} = OptionParser.parse(args,
+      switches: [name: :string]
+    )
+
+    options
+  end
+
   defp get_url body do
     %{"url" => url } = Poison.Parser.parse! body
-    IO.puts url
+    IO.puts "WebSocket URL: #{url}"
     url
   end
 
--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Butler.Mixfile do
     [app: :butler,
      version: "0.0.1",
      elixir: "~> 1.0",
+     escript: [main_module: Butler],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
