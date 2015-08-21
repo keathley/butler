cf04a9855ae400c6a659db3f4c1cef022da5a47c
Chris Keathley <c@keathley.io>
2015-08-21 17:17:07 -0400

    Remove explitives

--- lib/butler/ears.ex
+++ lib/butler/ears.ex
@@ -1,9 +1,4 @@
 defmodule Butler.Ears do
-
-  def hear("Hello Butler") do
-    {:reply, "Yo bitch"}
-  end
-
   def hear("cowsay " <> say) do
     {response, 0} = System.cmd("cowsay", [say])
     resp_string = "```#{response}```"
