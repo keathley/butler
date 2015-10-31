837fc14feb27659d9e05de5bdb65965dc2a082ea
Chris Keathley <c@keathley.io>
2015-10-30 23:59:37 -0400

    Update plugin example in generator

--- generator/templates/new/plugins/example.ex
+++ generator/templates/new/plugins/example.ex
@@ -1,8 +1,11 @@
 defmodule <%= mod_name %>.Example do
   use Butler.Plugin
 
-  # TODO - Add some comments with examples of how to bulid plugins
-  def respond("example me", state) do
-    {:reply, "This is an example response", state}
+  respond(~r/example me/) do
+    reply "This is an example response"
+  end
+
+  hear(~r/examples/) do
+    say "I heard some examples"
   end
 end
