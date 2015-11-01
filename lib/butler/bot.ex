5216dd8f777e07dde0d8d0837e2be2f86124087c
Chris Keathley <c@keathley.io>
2015-10-31 20:36:41 -0400

    Plugins now reference the bot module which returns a default name

--- lib/butler/bot.ex
+++ lib/butler/bot.ex
@@ -1,24 +1,30 @@
 defmodule Butler.Bot do
-  @adapter Application.get_env(:butler, :adapter)
-
   def notify(msg) do
     plugins
     |> Enum.each(fn(plugin) -> notify_plugin(plugin, msg) end)
   end
 
   def reply(resp) do
-    @adapter.reply(resp)
+    adapter.reply(resp)
   end
 
   def say(resp) do
-    @adapter.say(resp)
+    adapter.say(resp)
+  end
+
+  def name do
+    Application.get_env(:butler, :name) || "butler"
+  end
+
+  def plugins do
+    Application.get_env(:butler, :plugins)
+  end
+
+  def adapter do
+    Application.get_env(:butler, :adapter)
   end
 
   defp notify_plugin({plugin, _opts}, msg) do
     Task.Supervisor.start_child(Butler.PluginSupervisor, plugin, :notify, [msg])
-  end
-
-  defp plugins do
-    Application.get_env(:butler, :plugins)
   end
 end
