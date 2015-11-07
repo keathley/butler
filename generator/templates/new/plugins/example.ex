ce4c12f3261bb5d96019dc1c091508bb88202959
Bill Brock <william.e.brock@gmail.com>
2015-11-07 16:00:45 -0500

    updated examples to use new api

--- generator/templates/new/plugins/example.ex
+++ generator/templates/new/plugins/example.ex
@@ -1,11 +1,20 @@
 defmodule <%= mod_name %>.Example do
   use Butler.Plugin
 
-  respond(~r/example me/) do
-    reply "This is an example response"
+  @moduledoc """
+  replies to the user with an echo
+  """
+  respond ~r/echo (.*)/, conn, [_all, say] do
+
+    reply conn, "\necho: #{say}"
   end
 
-  hear(~r/examples/) do
-    say "I heard some examples"
+
+  @usage """
+  listens for a ping and broadcasts a pong
+  """
+  hear ~r/ping/, conn do
+    # ["test"|tests]
+    say conn, "-> pong"
   end
 end
