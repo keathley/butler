06d454a8225a56de9b854c641c868e64019427f2
Bill Brock <william.e.brock@gmail.com>
2015-11-08 20:15:13 -0500

    demonstrating different responders

--- generator/templates/new/plugins/example.ex
+++ generator/templates/new/plugins/example.ex
@@ -1,18 +1,28 @@
 defmodule <%= mod_name %>.Example do
   use Butler.Plugin
+  alias Butler.Plugin.Responders, as: Responder
 
   @moduledoc """
-  replies to the user with an echo
+  echoes back; demonstrates parsing basic user input
   """
   respond ~r/echo (.*)/, conn, [_all, say] do
-    reply conn, "echoing: #{say}"
+    reply conn, "#{say}"
   end
 
   @usage """
-  hears ping, says pong
+  hears ping, says pong; demonstrates basic listening and a broadcast
   """
   hear ~r/ping/, conn do
     # ["test"|tests]
     say conn, "-> pong"
+  end
+
+  @moduledoc """
+  replies back with code; demonstrates escaping code blocks
+  """
+  respond ~r/code/, conn do
+    response = "console.log('this should look like code!');"
+      |> Responder.code
+    reply conn, response
   end
 end
